14a7882085f0266df2d6204c3020368b026b2484
IT21313998 <99282379+IT21313998@users.noreply.github.com>
2022-05-20 21:48:38 +0530

    Add files via upload

---
+++ Nominee.h
@@ -0,0 +1,36 @@
+
+//#include"Event.h"
+class Nominee
+{
+protected:
+int  Nominee_ID;
+char Nominee_Name[20];
+char Symbol[30];
+
+public:
+	     Nominee();
+	     Nominee(int n_id,char n_name,char symbol[30]);
+		 void Submit_Nomination();
+		 void Cancel_Nomination();
+		 ~Nominee();
+};
+
+#include <cstring>
+#include "unregisterduser.h"
+unregisterduser::unregisterduser(){
+userID = 0;
+strcpy(userName, " ");
+strcpy(userAddress, " ");
+strcpy(userEmail, " ");
+userTel = 0;
+strcpy(userName, "");
+strcpy(password, "");
+}
+
+void unregisterduser::regisration()
+{
+}
+
+void unregisterduser::cancelRegistration()
+{
+}
---
+++ sponsor.h
@@ -0,0 +1,23 @@
+#include"organizer.h"
+#include"registerd.h"
+#include"system.h"
+class Sponsor
+{
+protected:
+
+
+public:
+	      Sponsor();
+		 void Advertise();
+		 ~ Sponsor();
+};
+#include <iostream>
+#include <cstring>
+#include "sponsor.h"
+
+Sponsor::Sponsor(){
+  
+}
+void Sponsor::Advertise(){
+  
+}
