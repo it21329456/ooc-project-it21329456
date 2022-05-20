234377775ecbab26683fb114b1fa9cbb24e406d7
it21329456 <99605648+it21329456@users.noreply.github.com>
2022-05-20 13:28:55 +0530

    Add files via upload

---
+++ registerd.h
@@ -0,0 +1,62 @@
+#include"Event.h"
+#include"Nominee.h"
+#include "unregisterduser.h"
+#include <cstring>"
+#include<iostream>
+
+class registerd: public unregisterduser
+{
+protected:
+int  V_IDR;
+char Name[20];
+char Password[30];
+//Class Relationship
+public:
+	     registerd();
+	     registerd(const char uname[25], const char cpwd[8]);
+		void login();
+		void passwordvalidation();
+		void logout();
+
+		 void Cast_Vote();
+		 void View_Result();
+		 ~registerd();
+};
+
+//implementation
+registerd::registerd()
+{
+strcpy(username, " ");
+strcpy(password, " ");
+}
+registerd::registerd(const char uname[25], const char cpwd[8])
+{
+strcpy(username, uname);
+strcpy(password, cpwd);
+}
+void registerd::login()
+{
+}
+void registerd::passwordvalidation()
+{
+}
+void registerd::logout()
+{
+}
+registerd::~registerd()
+{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ unregisterduser.h
@@ -0,0 +1,50 @@
+#include<iostream>
+#include <cstring>
+#include "unregisterduser.h"
+
+class unregisterduser
+{
+protected:
+
+int userID;
+char userName[30];
+char userAddress[30];
+char userEmail[30];
+int userTel;
+char username[10];
+char password[10];
+
+public:
+	     
+	     unregisterduser();
+		 unregisterduser(int uID,const char uname[],const char uaddress[],const char umail[],int utel[],const char pword[]);
+		 void regisration();
+		 void cancelRegistration();
+		 void displaydetails();
+		 char getName();
+		 int  getid();
+		 void View_Results();
+		 ~unregisterduser();
+};
+
+
+
+unregisterduser::unregisterduser(){
+userID = 0;
+strcpy(userName, " ");
+strcpy(userAddress, " ");
+strcpy(userEmail, " ");
+userTel = 0;
+strcpy(userName, "");
+strcpy(password, "");
+}
+
+void unregisterduser::regisration()
+{
+}
+
+void unregisterduser::cancelRegistration()
+{
+}
+  
+
